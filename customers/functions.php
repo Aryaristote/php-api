c2c96e90d167699ab4617da30bc27d45e0ed06f7
Peter NDENGO <frdrcpeter@gmail.com>
2024-02-16 13:24:51 +0200

    store new records

--- customers/functions.php
+++ customers/functions.php
@@ -1,5 +1,5 @@
 <?php
-
+error_reporting(0);
 require_once("../inc/dbcon.php");
 
 function getCustomerList() {
@@ -32,5 +32,49 @@ function getCustomerList() {
         ];
         http_response_code(500); 
         return json_encode($data);
+    }
+}
+
+//Create new customer.
+function error422 ($message) {
+    $data = [
+        'status' => '422',
+        'message' => $message,
+    ];
+    header('HTTP/1.0 422 Unprocessable Entity');
+    echo json_encode($data);
+    exit();
+}
+
+function storeCustomer($customerInput) {
+    global $conn;
+    $name = mysqli_real_escape_string($conn, $customerInput['name']);
+    $email = mysqli_real_escape_string($conn, $customerInput['email']);
+    $phone = mysqli_real_escape_string($conn, $customerInput['phone']);
+
+    if(empty(trim($name))) {
+        return error422('Enter your name');
+    }else if(empty(trim($email))){
+        return error422('Enter your mail address');
+    }else if(empty(trim($phone))){
+        return error422('Enter your phone number');
+    }else{
+        $query = "INSERT INTO customers (name, email, phone) VALUES ('$name', '$email', '$phone')";
+        $result = mysqli_query($conn, $query);
+        if($result){
+            $data = [
+                'status' => 201,
+                'message' => 'Customer created successfully'
+            ];
+            http_response_code(201); 
+            return json_encode($data);
+        }else {
+            $data = [
+                'status' => 500,
+                'message' => 'Internal Server Error'
+            ];
+            http_response_code(500); 
+            return json_encode($data);
+        }
     }
 }
